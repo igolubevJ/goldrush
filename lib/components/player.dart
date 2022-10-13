8dfc41175e1d9f8c5f8a0a3002c5f7ce8c6f93a4
igolubev <igolubevapp@gmail.com>
2022-10-13 20:20:30 +0300

    create player component

---
+++ lib/components/player.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+
+class Player {}
