d954604d309afea6073f7a9133020198554e98c2
igolubev <igolubevapp@gmail.com>
2022-10-13 20:39:23 +0300

    change position to squarePos

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -6,6 +6,11 @@ import 'package:flutter/material.dart';
 
 class Player extends PositionComponent {
   static const int squareSpeed = 250; // The speed that our square will animate
+  static final squarePaint = BasicPalette.green.paint();
+  static const  squareWidth = 100.0;
+  static const squareHeight = 100.0;
+
+  late Rect squarePos;
 
   // The direction our square is travelling in, 1 for left to right, -1 for
   // right to left
@@ -19,11 +24,11 @@ class Player extends PositionComponent {
 
     // Update the x position of the square based on the speed and direction
     // and the time elapsed
-    position.x += squareSpeed * squareDirection * dt;
+    squarePos = squarePos.translate(squareSpeed * squareDirection * dt, 0);
 
-    if (squareDirection == 1 && position.x > screenWidth) {
+    if (squareDirection == 1 && squarePos.right > screenWidth) {
       squareDirection = -1;
-    } else if (squareDirection == -1 && position.x < 0) {
+    } else if (squareDirection == -1 && squarePos.left < 0) {
       squareDirection = 1;
     }
   }
