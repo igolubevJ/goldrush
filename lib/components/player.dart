5b7cc9e6522d862fcead6ca4d94cb740b8f5b2ef
igolubev <igolubevapp@gmail.com>
2022-10-13 20:30:11 +0300

    implement update method

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -5,8 +5,21 @@ import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
 class Player extends PositionComponent {
+  static const int squareSpeed = 250; // The speed that our square will animate
+
+  // The direction our square is travelling in, 1 for left to right, -1 for
+  // right to left
+  int squareDirection = 1;
+
+
   @override
-  void update(double dt) {}
+  void update(double dt) {
+    super.update(dt);
+
+    // Update the x position of the square based on the speed and direction
+    // and the time elapsed
+    position.x += squareSpeed * squareDirection * dt;
+  }
 
   @override
   void render(Canvas canvas) {}
