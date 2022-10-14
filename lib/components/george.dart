f2dd1b2578ef0895badad41111fae33a9284b409
igolubev <igolubevapp@gmail.com>
2022-10-14 08:33:10 +0300

    Set up the screen variables, load the sprite sheet, set up sprite's position and size

--- lib/components/george.dart
+++ lib/components/george.dart
@@ -8,4 +8,27 @@ class George extends SpriteComponent {
   late double screenWidth, screenHeight, centerX, centerY;
   late double georgeSizeWidth = 48.0;
   late double georgeSizeHeight = 48.0;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    screenWidth = MediaQueryData.fromWindow(window).size.width;
+    screenHeight = MediaQueryData.fromWindow(window).size.height;
+
+    centerX = (screenWidth / 2) - (georgeSizeWidth / 2);
+    centerY = (screenHeight / 2) - (georgeSizeHeight / 2);
+
+    var spriteImages = await Flame.images.load('george.png');
+
+    final spriteSheet = SpriteSheet(
+      image: spriteImages,
+      srcSize: Vector2(georgeSizeWidth, georgeSizeHeight),
+    );
+
+    sprite = spriteSheet.getSprite(0, 0);
+
+    position = Vector2(centerX, centerY);
+    size = Vector2(georgeSizeWidth, georgeSizeHeight);
+  }
 }
