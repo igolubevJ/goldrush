d963afb51663ed9c92dbfbaf72b6810f1f211afd
igolubev <igolubevapp@gmail.com>
2022-10-14 16:18:29 +0300

    create Water constructor

--- lib/components/water.dart
+++ lib/components/water.dart
@@ -1,3 +1,11 @@
 import 'package:flame/components.dart';
 
-class Water extends PositionComponent with HasHitboxes, Collidable {}
+class Water extends PositionComponent with HasHitboxes, Collidable {
+  Water({
+    required Vector2 position,
+    required Vector2 size,
+    required this.id,
+  }) : super(position: position, size: size);
+
+  int id;
+}
