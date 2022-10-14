ccb8b4e5026a063dfeba81fdd1d01f40d77c795e
igolubev <igolubevapp@gmail.com>
2022-10-14 08:45:12 +0300

    override onLoad method for Background class

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -8,4 +8,15 @@ class Background extends PositionComponent {
   static final backgroundPaint = BasicPalette.white.paint();
 
   late double screenWidth, screenHeight;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    screenWidth = MediaQueryData.fromWindow(window).size.width;
+    screenHeight = MediaQueryData.fromWindow(window).size.height;
+
+    position = Vector2(0, 0);
+    size = Vector2(screenWidth, screenHeight);
+  }
 }
