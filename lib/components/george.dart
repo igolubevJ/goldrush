8f340ca4dc04613155f157c631e50e12f8f2a3ef
igolubev <igolubevapp@gmail.com>
2022-10-14 09:13:16 +0300

    create extension for work with column sprite sheet

--- lib/components/george.dart
+++ lib/components/george.dart
@@ -4,6 +4,28 @@ import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 
+extension CreateAnimationByColumn on SpriteSheet {
+  SpriteAnimation createAnimationByColumn({
+    required int column,
+    required double stepTime,
+    bool loop = true,
+    int from = 0,
+    int? to,
+  }) {
+    to ??= column;
+
+    final spriteList = List<int>.generate(to - from, (i) => from + i)
+        .map((e) => getSprite(e, column))
+        .toList();
+
+    return SpriteAnimation.spriteList(
+      spriteList,
+      stepTime: stepTime,
+      loop: loop,
+    );
+  }
+}
+
 class George extends SpriteAnimationComponent {
   late double screenWidth, screenHeight, centerX, centerY;
   late double georgeSizeWidth = 48.0;
