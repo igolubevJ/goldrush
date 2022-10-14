b0b40f2111c6f0170d3a6bc28c1d6a63d2fc3e78
igolubev <igolubevapp@gmail.com>
2022-10-14 10:22:16 +0300

    override onCollision for George

--- lib/components/george.dart
+++ lib/components/george.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flame/flame.dart';
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 
@@ -27,7 +28,7 @@ extension CreateAnimationByColumn on SpriteSheet {
   }
 }
 
-class George extends SpriteAnimationComponent {
+class George extends SpriteAnimationComponent with HasHitboxes, Collidable {
   static const int down = 0, left = 1, up = 2, right = 3;
 
   late double screenWidth, screenHeight, centerX, centerY;
@@ -76,6 +77,7 @@ class George extends SpriteAnimationComponent {
         spriteSheet.createAnimationByColumn(column: 3, stepTime: 0.2);
 
     changeDirection();
+    addHitbox(HitboxRectangle());
   }
 
   void changeDirection() {
@@ -98,6 +100,32 @@ class George extends SpriteAnimationComponent {
     }
 
     currentDirection = newDirection;
+  }
+
+  @override
+  void onCollision(Set<Vector2> points, Collidable other) {
+    if (other is ScreenCollidable) {
+      switch(currentDirection) {
+        case down:
+          currentDirection = up;
+          animation = georgeUpAnimation;
+          break;
+        case left:
+          currentDirection = right;
+          animation = georgeRightAnimation;
+          break;
+        case up:
+          currentDirection = down;
+          animation = georgeUpAnimation;
+          break;
+        case right:
+          currentDirection = left;
+          animation = georgeLeftAnimation;
+          break;
+      }
+
+      elapsedTime = 0.0;
+    }
   }
 
   @override
