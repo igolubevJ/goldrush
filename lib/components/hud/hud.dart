2cc05595f0b51df3e1a73af85aaaa9ba5220388f
igolubev <igolubevapp@gmail.com>
2022-10-14 12:10:02 +0300

    create constructor HudComponent

--- lib/components/hud/hud.dart
+++ lib/components/hud/hud.dart
@@ -2,4 +2,6 @@ import 'package:flame/palette.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class HudComponent extends PositionComponent {}
+class HudComponent extends PositionComponent {
+  HudComponent() : super(priority: 20);
+}
