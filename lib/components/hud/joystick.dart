1ecd0526d345b448cec34755524d8a2a5aa7c8b5
igolubev <igolubevapp@gmail.com>
2022-10-14 12:07:12 +0300

    add Joystick controller

--- lib/components/hud/joystick.dart
+++ lib/components/hud/joystick.dart
@@ -1,5 +1,10 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Joystick extends JoystickComponent {}
-
+class Joystick extends JoystickComponent {
+  Joystick({
+    required PositionComponent knob,
+    PositionComponent? background,
+    EdgeInsets? margin,
+  }) : super(knob: knob, background: background, margin: margin);
+}
