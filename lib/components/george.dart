e0f5e5ecc4ce00f87f61274bdbbe363bfde7e285
igolubev <igolubevapp@gmail.com>
2022-10-14 08:22:36 +0300

    create sprite component George

---
+++ lib/components/george.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flame/flame.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+
+class George extends SpriteComponent {}
