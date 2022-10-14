4b57e31a88b90ff71de8ace5b59778654ce3d787
igolubev <igolubevapp@gmail.com>
2022-10-14 08:40:26 +0300

    create Background Position Component

---
+++ lib/components/background.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flame/flame.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Background extends PositionComponent {}
