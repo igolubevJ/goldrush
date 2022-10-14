6946ad17c1083de2a00efc527425ec50209e4543
igolubev <igolubevapp@gmail.com>
2022-10-14 11:29:35 +0300

    add RunButton constructor

--- lib/components/hud/run_button.dart
+++ lib/components/hud/run_button.dart
@@ -1,3 +1,23 @@
+import 'package:flame/components.dart';
 import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
 
-class RunButton extends HudButtonComponent {}
+class RunButton extends HudButtonComponent {
+  RunButton({
+    required button,
+    buttonDown,
+    EdgeInsets? margin,
+    Vector2? position,
+    Vector2? size,
+    Anchor anchor = Anchor.center,
+    onPressed,
+  }) : super(
+          button: button,
+          buttonDown: buttonDown,
+          margin: margin,
+          position: position,
+          size: size ?? button.size,
+          anchor: anchor,
+          onPressed: onPressed,
+        );
+}
