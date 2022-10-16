539e71eb5353b7a892c7cdc8db53dc84a9ac27e8
igolubev <igolubevapp@gmail.com>
2022-10-16 22:13:05 +0300

    change onLoad main

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ import 'package:goldrush/components/coin.dart';
 import 'package:goldrush/components/george.dart';
 import 'package:goldrush/components/hud/hud.dart';
 import 'package:goldrush/components/skeleton.dart';
+import 'package:goldrush/components/tailemap.dart';
 import 'package:goldrush/components/water.dart';
 import 'package:goldrush/components/zombie.dart';
+import 'package:goldrush/utils/math_utils.dart';
 
 void main() async {
   // Create an instance of the game
@@ -32,41 +34,41 @@ class GoldRush extends FlameGame
   Future<void> onLoad() async {
     super.onLoad();
 
-    // debugMode = true;
+    Rect gameScreenBounds = getGameScreenBounds(canvasSize);
 
-    HudComponent hud = HudComponent();
-    George george = George(
-      hud: hud,
-      position: Vector2(200, 400),
-      size: Vector2(48.0, 48.0),
-      speed: 40.0,
-    );
-
-    add(Background(george));
-    final tiledMap = await TiledComponent.load('tiles.tmx', Vector2.all(32));
-
-    add(tiledMap);
+    var hud = HudComponent();
+    var george = George(
+        hud: hud,
+        position: Vector2(
+          gameScreenBounds.left + 300,
+          gameScreenBounds.top + 300,
+        ),
+        size: Vector2(48.0, 48.0),
+        speed: 40.0);
     add(george);
 
-    final enemies = tiledMap.tileMap.getObjectGroupFromLayer('Enemies');
+    children.changePriority(george, 15);
 
+    add(Background(george));
+
+    final tiledMap = await TiledComponent.load('tiles.tmx', Vector2.all(32));
+    add(TileMapComponent(tiledMap));
+    add(hud);
+
+    final enemies = tiledMap.tileMap.getObjectGroupFromLayer('Enemies');
     enemies.objects.asMap().forEach((index, position) {
       if (index % 2 == 0) {
-        add(
-          Skeleton(
-            position: Vector2(position.x, position.y),
+        add(Skeleton(
+            position: Vector2(position.x + gameScreenBounds.left,
+                position.y + gameScreenBounds.top),
             size: Vector2(32.0, 64.0),
-            speed: 60.0,
-          ),
-        );
+            speed: 60.0));
       } else {
-        add(
-          Zombie(
-            position: Vector2(position.x, position.y),
+        add(Zombie(
+            position: Vector2(position.x + gameScreenBounds.left,
+                position.y + gameScreenBounds.top),
             size: Vector2(32.0, 64.0),
-            speed: 20.0,
-          ),
-        );
+            speed: 20.0));
       }
     });
 
@@ -74,31 +76,24 @@ class GoldRush extends FlameGame
     for (int i = 0; i < 50; i++) {
       int randomX = random.nextInt(48) + 1;
       int randomY = random.nextInt(48) + 1;
+      double posCoinX = (randomX * 32) + 5 + gameScreenBounds.left;
+      double posCoinY = (randomY * 32) + 5 + gameScreenBounds.top;
 
-      double posCoinX = (randomX * 32) + 5;
-      double posCoinY = (randomY * 32) + 5;
-
-      add(Coin(
-        position: Vector2(posCoinX, posCoinY),
-        size: Vector2(20, 20),
-      ));
+      add(Coin(position: Vector2(posCoinX, posCoinY), size: Vector2(20, 20)));
     }
 
     final water = tiledMap.tileMap.getObjectGroupFromLayer('Water');
     water.objects.forEach((rect) {
       add(Water(
-        position: Vector2(rect.x, rect.y),
-        size: Vector2(rect.width, rect.height),
-        id: rect.id,
-      ));
+          position: Vector2(
+              rect.x + gameScreenBounds.left, rect.y + gameScreenBounds.top),
+          size: Vector2(rect.width, rect.height),
+          id: rect.id));
     });
 
-    add(hud);
-
     camera.speed = 1;
-    camera.followComponent(
-      george,
-      worldBounds: const Rect.fromLTWH(0, 0, 1600, 1600),
-    );
+    camera.followComponent(george,
+        worldBounds: Rect.fromLTWH(
+            gameScreenBounds.left, gameScreenBounds.top, 1600, 1600));
   }
 }
